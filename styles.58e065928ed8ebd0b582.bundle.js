f4f4cfa151774062eddc7980424c457cbd5f2a70
Igor N. Dultsev <yhaskell@live.com>
2016-12-12 05:45:16 +0700

    fixed

--- styles.58e065928ed8ebd0b582.bundle.js
+++ styles.58e065928ed8ebd0b582.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{450:function(n,o){},451:function(n,o){},797:function(n,o,c){c(450),n.exports=c(451)}},[797]);
+webpackJsonp([1,3],{450:function(n,o){},451:function(n,o){},797:function(n,o,c){c(450),n.exports=c(451)}},[797]);
 //# sourceMappingURL=styles.58e065928ed8ebd0b582.bundle.map
